60617ea0f609318ee8f802c1fe2998f423ca1713
lu.teddy <lu.teddy@acdafdce-8c61-a86e-f6dd-5253cd6aaa21>
2010-10-04 19:22:50 +0000

    Hice funciones genericas para indicar que el comando esta en ejecucion y que fepini ya fue ejecutado

--- trunk/comandos/fepini.sh
+++ trunk/comandos/fepini.sh
@@ -1,12 +1,85 @@
 #!/bin/bash
 
-#fepini
+#	Todos las las funciones devuelven 
+#	0	<->	OK
+#	1	<-> ERROR
+
+#################################################
+#	guarda que el comando esta siendo ejecutado	#
+#	param: nombre del comando					#
+#################################################
+bloquear(){
+	if [ $# -lt 1 ]
+	then
+		echo "*** bloquear recibe el nombre del comando ***"
+		return 1
+	fi
+	
+	local comando=`basename $1`
+	if [ -a "$grupo/temp/.running_$comando.lck" ]
+	then
+		echo "El comando $comando ya esta siendo ejecutado."
+		#Glog
+		return 1
+	else
+		echo $! > "$grupo/temp/.running_$comando.lck"
+		echo Creado archivo lock "$grupo/temp/.running_$comando.lck" #TODO borrar esta linea
+		return 0
+	fi
+}
+
+#############################
+#	 borrar archivo lock	#
+# 	 param: nombre comando	#
+#############################
+desbloquear(){
+	if [ $# -lt 1 ]
+	then
+		echo "*** desbloquear recibe el nombre del comando ***"
+		return 1
+	fi
+	
+	local comando=`basename $1`
+	rm -f "$grupo/temp/.running_$comando.lck" > /dev/null
+	return $?
+}
+
+#############################################
+#	indica si se puede ejecutar el comando	#
+#	param: nombre del comando				#
+#############################################
+iniTests(){
+	if [ $# -lt 1 ]
+	then
+		echo "*** iniTest recibe el nombre del comando ***"
+		return 1
+	fi
+	
+	if [ -z $INI_FEPINI ]
+	then
+		echo No se ha inicializado el ambiente. Debe ejecutarse el comando \". fepini.sh\" previamente. 
+		#Glog -se "No se ha inicializado el ambiente. Debe ejecutarse el comando \". fepini.sh\" previamente. "
+		return 1
+	fi
+	
+	bloquear $1
+	return $?
+}
+
+
+##########################
+#	fepini				 #
+##########################
+
 
 if [ ! -z $INI_FEPINI ]
 then
 	echo El ambiente ya ha sido inicializado 
 	return 1
 fi
+
+
+#	Variables de Ambiente	#
 
 export INI_FEPINI=1			# indica que fepini esta siendo ejecutado
 export grupo="$PWD/.."		# directorio del trabajo practico 
@@ -18,7 +91,11 @@ export RECHAZADOS="$grupo/rechazados"
 echo "La variable grupo es $grupo"
 echo "La variable PATH es $PATH"
 
+#	Funciones genericas para todos los comandos	#
 
+export -f iniTests
+export -f bloquear
+export -f desbloquear
 
 
 return 0
