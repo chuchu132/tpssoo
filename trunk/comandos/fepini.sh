c411d3d4622da5fc0f99d36dac69b79d44861375
lu.teddy <lu.teddy@acdafdce-8c61-a86e-f6dd-5253cd6aaa21>
2010-10-04 04:09:02 +0000

    Feprima crea un archivo lock en la carpeta temp para saber si esta siendo ejecutado. TEngo problemas con el path :P despues veo como hago =p

--- trunk/comandos/fepini.sh
+++ trunk/comandos/fepini.sh
@@ -10,7 +10,7 @@ fi
 
 export INI_FEPINI=1			# indica que fepini esta siendo ejecutado
 export grupo="$PWD/.."		# directorio del trabajo practico 
-export PATH="$PATH:$PWD"
+export PATH="$PATH:$PWD"	#TODO no me esta tomando las rutas con espacios!
 export RECIBIDOS="$grupo/recibidos"
 export ACEPTADOS="$grupo/aceptados"
 export RECHAZADOS="$grupo/rechazados"
